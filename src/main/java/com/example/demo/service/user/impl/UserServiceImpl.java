899d22b0855205cbbb8e77f4d18eda30e8d85049
xin11.liu <xin11.liu@nttdata.com>
2022-10-28 15:35:04 +0900

    修正

--- src/main/java/com/example/demo/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/user/impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package com.example.demo.service.user.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.demo.entities.User;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.user.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
